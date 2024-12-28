8167ed0c34263f127809cbae657747f102ef2f97
Chetan <chetanfern@gmail.com>
2024-12-28 15:02:53 +0530

    Second

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(file_path:str)->List[str]:
     
         return requirements
 setup(
-    name="Mlflow",
+    name="Phishing",
     version="0.0.1",
     author="Chetan",
     author_email="chetanfern@gmail.com",
